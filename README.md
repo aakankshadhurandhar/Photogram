464c6ed61909050859f972279d887df1b816bc25
Aakanksha Dhurandhar <34390765+aakankshadhurandhar@users.noreply.github.com>
2021-04-20 18:42:12 +0530

    updated readme

--- README.md
+++ README.md
@@ -8,11 +8,19 @@
 * Pwa app(progressive web app)-React app can be installed in various devices and used
 * Local storage for themes
 * Delete photos
+* Signin through google
+* Only Registered users can delete/upload photos
   
 # Live Website Link
 [Photogram](https://photogram34.netlify.app/)
 
-![demo](screenshots/Kazam_screencast_00003%20(online-video-cutter.com).gif)
+![web demo](screenshots/664ecc41-892e-4f7d-89ba-dee4706c52e3.gif)
+
+![mobile/app demo](screenshots/WhatsApp%20Video%202021-04-20%20at%206.34.49%20PM.gif)
 
 
 
+## Future Updates-
+* Search for any image by specific user
+* add save button(like instagram)
+  
--- src/firebase/config.js
+++ src/firebase/config.js
@@ -7,12 +7,13 @@ import 'firebase/firestore'
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC3GjU2NJIK84eDokePDRio1-zO5luIB8w",
-    authDomain: "photogram-35812.firebaseapp.com",
-    projectId: "photogram-35812",
-    storageBucket: "photogram-35812.appspot.com",
-    messagingSenderId: "286383348279",
-    appId: "1:286383348279:web:770e0c07dd833fc800fdb2"
+  apiKey: "AIzaSyC3GjU2NJIK84eDokePDRio1-zO5luIB8w",
+  authDomain: "photogram-35812.firebaseapp.com",
+  databaseURL: "https://photogram-35812-default-rtdb.firebaseio.com",
+  projectId: "photogram-35812",
+  storageBucket: "photogram-35812.appspot.com",
+  messagingSenderId: "286383348279",
+  appId: "1:286383348279:web:50abb0d5507d99cb00fdb2"
   };
   const firebaseApp = firebase.initializeApp(firebaseConfig);
   const db = firebaseApp.firestore();
